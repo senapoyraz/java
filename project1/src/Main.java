96919abe079517eb422265861fcec351e29a7bfe
senapoyraz <senapoyraz050@gmail.com>
2026-03-10 16:44:06 +0300

    add al changes

---
+++ project1/src/Main.java
@@ -0,0 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("fizik notunuzu giriniz:");
+        double fizik = input.nextDouble();
+        System.out.println("kimya notunuzu giriniz:");
+        double kimya = input.nextDouble();
+        System.out.println("biyoloji notunuzu giriniz:");
+        double biyoloji = input.nextDouble();
+        System.out.println("mat notunuzu giriniz:");
+        double mat = input.nextDouble();
+        double ort = (fizik + kimya + biyoloji + mat) / (double)4.0F;
+        System.out.println("Not ortalamaniz: " + ort);
